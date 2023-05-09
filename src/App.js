b80ba539687ec1dea457a923324875bf5dd023e6
Mohammed Shaikh <shaikh.adnan@hotmail.com>
2023-05-09 14:52:04 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>API PROJECT INCOMING</h1>
+      <h2>The app isnt completed yet, but when it will be finished.... ✨amazing✨</h2>
       </header>
     </div>
   );
