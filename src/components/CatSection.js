6500248f9b2af12e31e83094784c0c12b97775b8
Mohammed Shaikh <shaikh.adnan@hotmail.com>
2023-05-11 22:09:10 -0400

    fetched API to get image and update dropdown

--- src/components/CatSection.js
+++ src/components/CatSection.js
@@ -1,12 +1,48 @@
 // API KEY =
 // live_j29E1vec9gYqx9oyno1sprzIEcYTs7Vwh8DXzyUR9qRYl1M6E57JzcveakP1JCbh
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import AnimalInfo from "./AnimalInfo";
+import axios from "axios";
+
+//'https://api.thecatapi.com/v1/breeds'
 
 const CatSection = () => {
 
     const [catInfo, setCatInfo] = useState("");
+
+    const [breeds, setBreeds] = useState([]);
+
+    const [images, setImages] = useState([])
+
+    useEffect(()=>{
+        axios({
+            url: 'https://api.thecatapi.com/v1/breeds',
+            params: {
+                api_key: "live_j29E1vec9gYqx9oyno1sprzIEcYTs7Vwh8DXzyUR9qRYl1M6E57JzcveakP1JCbh"
+            }
+        }).then((response)=>{
+            const breedArray = (response.data)
+            setBreeds(breedArray);
+        })
+    },[]);
+
+    
+
+    useEffect(()=>{
+        axios({
+            url: "https://api.thecatapi.com/v1/images/search",
+            params:{
+                api_key: "live_j29E1vec9gYqx9oyno1sprzIEcYTs7Vwh8DXzyUR9qRYl1M6E57JzcveakP1JCbh",
+                breed_ids: "abys"
+            }
+            
+        }).then((response)=>{
+            const image = (response.data)
+            setImages(image);
+            console.log(image);
+        })
+    },[])
 
     // have it so when person clicks on the learn more, the new component of animalInfo shows up.
 
@@ -16,27 +52,33 @@ const CatSection = () => {
         setCatInfo(infoButton);
     }
 
+    const handleChange = (event) => {
+        const value = (event.target.value)
+        console.log(value)
+    }
 
 
-    fetch('https://api.thecatapi.com/v1/breeds')
-        .then((result) => {
-            return result.json()
-        })
-        .then((data) => {
-            const catData = (data);
-            console.log(catData);
-        })
-
-
+        
     return (
         <div className="catSection">
-            <form>
+            <form onChange={handleChange}>
                 <label htmlFor="catsBreed">Choose a Breed:</label>
                 <select name="catsBreed" id="catsBreed">
-                    <option value="beng">Bengal</option>
+                    {breeds.map((breed)=>{
+                        return(
+                            <option key={breed.id} value={breed.id} id={breed.reference_image_id}>{breed.name}</option>
+                        )
+                    })}
                 </select>
             </form>
-            <img src="http://placekitten.com/280/300" alt="a cat" />
+                {images.map((image)=>{
+                    return(
+                        <img key={image.breeds[0].name} src={image.url} alt={image.breeds[0].name} />
+                    )
+                })
+
+                }
+            
             <button onClick={handleInfoClick}>LEARN MORE</button>
             {
                 catInfo === "LEARN MORE"
