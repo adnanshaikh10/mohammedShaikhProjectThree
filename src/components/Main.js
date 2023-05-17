708abcd202f983384f4898f59f057992aa02fc5f
Mohammed Shaikh <shaikh.adnan@hotmail.com>
2023-05-17 17:42:02 -0400

    Minor change for app appearance

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import React from "react";
 import CatSection from "./CatSection";
 import DogSection from "./DogSection";
-import CatImage from "../assets/CatImage.jpeg";
+import CatImage from "../assets/CatImage.jpg";
 import DogImage from "../assets/DogImage.jpg";
 
 const Main = () => {
@@ -17,11 +17,11 @@ const Main = () => {
         <main>
             <div className="choosePet">
                 <div className="catTeam">
-                    <img src= {CatImage} alt="Archie"></img>
+                    <img src= {CatImage} alt="Riley sitting on a roku remote"></img>
                     <button onClick={handleAnimalClick}>CATS</button>
                 </div>
                 <div className="dogTeam">
-                    <img src={DogImage} alt="Nyx"></img>
+                    <img src={DogImage} alt="Marley smiling wearing a blue bandana"></img>
                     <button onClick={handleAnimalClick}>DOGS</button>
                 </div>
             </div>
